83e0ffe140a6a60fc1aa128a0df997a446fd9bb8
mararo-ops <danielmararo1@gmail.com>
2023-12-13 18:01:59 +0300

    complete assignment

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,48 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items=items
+        self.limit =limit
 
     def isEmpty(self):
-        pass
+        #checking if the stack is empty
+        return len(self.items) ==0
 
     def push(self, item):
-        pass
-
+        #pushing an item onto the stack
+        if len(self.items) <self.limit:
+            self.items.append(item)
+        else:
+            return None #no action if the stack is full
+                
+        
     def pop(self):
-        pass
-
+        #pop an item from the stack
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            return None
+  
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            return None
+  
     
     def size(self):
-        pass
+    # """Get the number of items in the stack."""
+        return len(self.items)
 
     def full(self):
-        pass
+     #    """Check if the stack is full."""
+        return len(self.items) == self.limit
+
 
     def search(self, target):
-        pass
+         #"""Search for an item in the stack and return its distance from the top."""
+        try:
+            index = self.items[::-1].index(target)  # Reverse the items list to search from the top
+            return len(self.items) - index  # Calculate distance from the top
+        except ValueError:
+            return None  # Return None if the item is not found
